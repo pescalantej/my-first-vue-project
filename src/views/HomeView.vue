f8710b2b72562983853e8299a07cb1c4ede64964
Pablo Escalante <pescalantej@gmail.com>
2023-05-11 16:15:25 -0400

    Ep 21, Refactoring to defineEmits and defineProps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup>
-import { useStorage } from '@/composables/useStorage'
+import TabbableTextarea from '@/components/TabbableTextarea.vue'
+import { ref } from 'vue'
 
-let food = useStorage('food', 'salad')
-let age = useStorage('age', '37')
+let comment = ref('test value')
+
+setTimeout(() => {
+  comment.value = 'it works!'
+}, 2000)
 </script>
 
 <template>
-  <div>
-    <p>What is your favorite food? <input type="text" v-model="food" /></p>
-    <p>How old are you? <input type="text" v-model="age" /></p>
-  </div>
+  <main>
+    <form>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px" />
+    </form>
+  </main>
 </template>
