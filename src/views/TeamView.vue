62bce0f20cb38cf9131297bf16bea59a7b05d99c
Pablo Escalante <pescalantej@gmail.com>
2023-05-18 16:17:09 -0400

    Build a Modal Component

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,24 +2,38 @@
 import TeamMembers from '@/components/Teams/TeamMembers.vue'
 import TeamHeader from '@/components/Teams/TeamHeader.vue'
 import TeamFooter from '@/components/Teams/TeamFooter.vue'
+import Modal from '@/components/Modal.vue'
 
 import { useTeamStore } from '@/stores/TeamStore'
+
+import { ref } from 'vue'
 
 let team = useTeamStore()
 
 team.fill()
 
-setTimeout(() => {
-  team.grow(25)
-}, 2000)
+let showModal = ref(false)
 </script>
 
 <template>
-  <TeamHeader />
+  <TeamHeader @add="showModal = true" />
 
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamMembers />
   </div>
 
   <TeamFooter />
+
+  <Modal :show="showModal" @close="showModal = false">
+    <template #default>
+      <p>Need to add a new member to your team?</p>
+
+      <form class="mt-6">
+        <div class="flex gap-2">
+          <input type="email" placeholder="Email address..." class="flex-1" />
+          <button>Add</button>
+        </div>
+      </form>
+    </template>
+  </Modal>
 </template>
